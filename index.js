3e52900564c956e8370355697c1e1e81323e600f
imabigboynow <45216640+imabigboynow@users.noreply.github.com>
2018-12-06 15:36:19 -0800

    The god edit

--- index.js
+++ index.js
@@ -1,80 +1,118 @@
-class Character {
-  constructor(x, y, color, radius, speed) {
-    Object.assign(this, { x, y, color, radius, speed });
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+const progressBar = document.querySelector("progress");
+
+function startGame() {
+  if (progressBar.value === 0) {
+    progressBar.value = 100;
+    Object.assign(player, { x: canvas.width / 2, y: canvas.height / 2 });
+    requestAnimationFrame(drawScene);
+  }
+}
+
+function distanceBetween(sprite1, sprite2) {
+  return Math.hypot(sprite1.x - sprite2.x, sprite1.y - sprite2.y);
+}
+
+function haveCollided(sprite1, sprite2) {
+  return distanceBetween(sprite1, sprite2) < sprite1.radius + sprite2.radius;
+}
+
+class Sprite {
+  draw() {
+    ctx.fillStyle = this.color;
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    ctx.fill();
+    ctx.stroke();
+  }
+}
+
+class Player extends Sprite {
+  constructor(x, y, radius, color, speed) {
+    super();
+    this.image = new Image();
+    this.image.src = "https://vignette.wikia.nocookie.net/callofduty/images/f/f4/Soldier_sprite_DOA_BO.png/revision/latest?cb=20121107192309";
+    Object.assign(this, { x, y, radius, color, speed });
   }
   draw() {
-    fill(this.color);
-    ellipse(this.x, this.y, this.radius * 2);
-  }
-  move(target) {
-    this.x += (target.x - this.x) * this.speed;
-    this.y += (target.y - this.y) * this.speed;
+    ctx.drawImage(this.image, this.x, this.y, 50, 50);
   }
 }
 
-const player = new Character(30, 30, "blue", 10, 0.05);
-const enemies = [
-  new Character(300, 0, "rgb(200,190,80)", 15, 0.01),
-  new Character(300, 300, "rgb(240,100,250)", 17, 0.03),
-  new Character(0, 300, "rgb(80,200,235)", 20, 0.003),
-  new Character(20, 400, "rgb(100,170,190)", 12, 0.02),
+let player = new Player(250, 150, 10, "black", 0.07);
+
+class Enemy extends Sprite {
+  constructor(x, y, radius, color, speed) {
+    super();
+    Object.assign(this, { x, y, radius, color, speed });
+  }
+}
+
+let enemies = [
+  new Enemy(80, 200, 20, "rgba(255, 0, 0, 0.8)", 0.07),
+  new Enemy(200, 250, 17, "rgba(255, 0, 0, 0.7)", 0.03),
+  new Enemy(150, 180, 22, "rgba(255, 0, 0, 0.5)", 0.007),
 ];
-let scarecrow;
 
-//source:https://www.youtube.com/watch?v=IXTJlH7g0tw
-let clickNoise= new Audio(
-  "https://raw.githubusercontent.com/imabigboynow/zombiechasegame/master/tacticalnuke.wav"
-);
-
-function setup()  {
-  createCanvas(400, 400);
-  noStroke();
+let mouse = { x: 0, y: 0 };
+document.body.addEventListener("mousemove", updateMouse);
+function updateMouse(event) {
+  const { left, top } = canvas.getBoundingClientRect();
+  mouse.x = event.clientX - left;
+  mouse.y = event.clientY - top;
 }
 
-function draw() {
-  background("lightgreen");
-  player.draw();
-  player.move({x: mouseX, y: mouseY});
-  enemies.forEach(enemy => enemy.draw());
-  enemies.forEach(enemy => enemy.move(scarecrow || player));
-  if (scarecrow) {
-    scarecrow.draw();
-    scarecrow.ttl--; 
-    if (scarecrow.ttl < 0) {
-      scarecrow = undefined;
-    }
-  }
-  adjust();
-}
 
-function adjust() {
-  const characters = [player, ...enemies];
-  for (let i = 0; i < characters.length; i++) {
-    for (let j = i+1; j < characters.length; j++) {
-      pushOff(characters[i], characters[j]);
-    }
-  }
+function moveToward(leader, follower, speed) {
+  follower.x += (leader.x - follower.x) * speed;
+  follower.y += (leader.y - follower.y) * speed;
 }
 
 function pushOff(c1, c2) {
   let [dx, dy] = [c2.x - c1.x, c2.y - c1.y];
-  const distance = Math.hypot(dx, dy);
-  let overlap = c1.radius + c2.radius - distance;
-  if (overlap > 0) {
-    const adjustX = (overlap / 2) * (dx / distance);
-    const adjustY = (overlap / 2) * (dy / distance);
-    c1.x -= adjustX;
-    c1.y -= adjustY;
-    c2.x += adjustX;
-    c2.y += adjustY;
+  const L = Math.hypot(dx, dy);
+  let distToMove = c1.radius + c2.radius - L;
+  if (distToMove > 0) {
+    dx /= L;
+    dy /= L;
+    c1.x -= dx * distToMove / 2;
+    c1.y -= dy * distToMove / 2;
+    c2.x += dx * distToMove / 2;
+    c2.y += dy * distToMove / 2;
   }
 }
 
-function mouseClicked() {
-  if (!scarecrow) {
-    scarecrow = new Character(player.x, player.y, "white", 10, 0);
-    scarecrow.ttl = frameRate() * 5;
-    clickNoise.currentTime = 0;
-    clickNoise.play();
+function updateScene() {
+  moveToward(mouse, player, player.speed);
+  enemies.forEach(enemy => moveToward(player, enemy, enemy.speed));
+  enemies.forEach((enemy, i) =>
+    pushOff(enemy, enemies[(i + 1) % enemies.length])
+  );
+  enemies.forEach(enemy => {
+    if (haveCollided(enemy, player)) {
+      progressBar.value -= 2;
+    }
+  });
+}
+
+function clearBackground() {
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+function drawScene() {
+  clearBackground();
+  player.draw();
+  enemies.forEach(enemy => enemy.draw());
+  updateScene();
+  if (progressBar.value <= 0) {
+    ctx.font = "25px Comic Sans MS";
+    ctx.fillText("Tango down click to play again", 10, canvas.height / 2);
+  } else {
+    requestAnimationFrame(drawScene);
   }
 }
+
+canvas.addEventListener("click", startGame);
+requestAnimationFrame(drawScene);
